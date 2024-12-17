6bdd5ee349c9a627fcccad8ede637a1cdc797b3f
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-17 15:57:19 +0800

    chore: undo auth setup

--- web-api/Program.cs
+++ web-api/Program.cs
@@ -1,7 +1,5 @@
 using Microsoft.EntityFrameworkCore;
 using TodoApi.Models;
-using Pomelo.EntityFrameworkCore.MySql;
-using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,17 +13,6 @@ builder.Services.AddOpenApi();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    Env.Load(".env.development");
-}
-else
-{
-    Env.Load(".env.production");
-}
-
 var dbConnectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
 
 builder.Services.AddDbContext<TodoContext>(options =>
@@ -34,32 +21,7 @@ builder.Services.AddDbContext<TodoContext>(options =>
         new MySqlServerVersion(new Version(8, 0, 36))
     ));
 
-// JWT setup
-var jwtSecretKey = Environment.GetEnvironmentVariable("JWT_SECRETKEY");
-var jwtIssuer = Environment.GetEnvironmentVariable("JWT_ISSUER");
-var jwtAudience = Environment.GetEnvironmentVariable("JWT_AUDIENCE");
-
-if (string.IsNullOrEmpty(jwtSecretKey))
-{
-    throw new InvalidOperationException("JWT_SECRETKEY is required but is not set.");
-}
-
-// Add authentication using JWT
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer(options =>
-    {
-        options.RequireHttpsMetadata = false;
-        options.SaveToken = true;
-        options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-        {
-            ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidIssuer = jwtIssuer,
-            ValidAudience = jwtAudience,
-            IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(jwtSecretKey))
-        };
-    });
+var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
