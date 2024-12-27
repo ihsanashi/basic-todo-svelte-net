fff5b980d90aa899fa2970b43c9183e0767d8c99
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-27 23:34:20 +0800

    chore: add todo endpoint url

--- client/src/references/constants/index.ts
+++ client/src/references/constants/index.ts
@@ -2,3 +2,4 @@ export * from './messages';
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const API_AUTH_URL = `${API_BASE_URL}/auth`;
+export const API_TODO_URL = `${API_BASE_URL}/TodoItems`;
