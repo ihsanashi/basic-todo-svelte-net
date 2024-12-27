9770973e27ea718d3d5093f8ba2d13fde7fa426f
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-27 12:11:47 +0800

    chore: make email a required field

---
+++ web-api/Migrations/20241227030703_MakeEmailNonNullable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeEmailNonNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "AspNetUsers",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldNullable: false
+            );
+        }
+    }
+}
