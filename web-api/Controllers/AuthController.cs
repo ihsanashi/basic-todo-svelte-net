c42f87f0e69319bf764ac04b98d66bb988290f0f
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-27 12:20:56 +0800

    feat: add me endpoint for user details

--- web-api/Controllers/AuthController.cs
+++ web-api/Controllers/AuthController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using TodoApi.Extensions;
+using TodoApi.Models;
 
 namespace TodoApi.Controllers
 {
@@ -12,10 +13,12 @@ namespace TodoApi.Controllers
   public class AuthController : ControllerBase
   {
     private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
 
-    public AuthController(SignInManager<IdentityUser> signInManager)
+    public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
     {
       _signInManager = signInManager;
+      _userManager = userManager;
     }
 
     /// <summary>
@@ -40,6 +43,23 @@ namespace TodoApi.Controllers
       }
 
       return StatusCode(500, new { Message = errorMessage });
+    }
+
+    [HttpGet("me")]
+    [ProducesResponseType(typeof(UserDto), 200)]  // Specifies the return type as UserDto
+    [ProducesResponseType(typeof(object), 401)]  // Unauthorized response
+    [ProducesResponseType(typeof(object), 500)]  // Internal Server Error response
+
+    public async Task<IActionResult> GetCurrentUser()
+    {
+      var (success, user, errorMessage) = await IdentityExtension.GetCurrentUserAsync(HttpContext, _userManager);
+
+      if (success)
+      {
+        return Ok(user);
+      }
+
+      return Unauthorized(new { Message = errorMessage });
     }
   }
 }
--- web-api/Extensions/IdentityExtension.cs
+++ web-api/Extensions/IdentityExtension.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+using TodoApi.Models;
 
 namespace TodoApi.Extensions
 {
@@ -20,6 +22,32 @@ namespace TodoApi.Extensions
       {
         return (false, $"An error occurred during logout: {ex.Message}");
       }
+    }
+
+    public static async Task<(bool success, UserDto? user, string? errorMessage)> GetCurrentUserAsync(HttpContext httpContext, UserManager<IdentityUser> userManager)
+    {
+      var userClaims = httpContext.User;
+      if (userClaims.Identity?.IsAuthenticated == true)
+      {
+        var userId = userClaims.FindFirstValue(ClaimTypes.NameIdentifier);
+        if (userId == null)
+        {
+          return (false, null, "User ID not found in the claims.");
+        }
+
+        var user = await userManager.FindByIdAsync(userId);
+        if (user != null)
+        {
+          var userDto = UserDto.MapToUserDto(user);
+          return (true, userDto, null);
+        }
+        else
+        {
+          return (false, null, "User not found.");
+        }
+      }
+
+      return (false, null, "User is not authenticated.");
     }
   }
 }
