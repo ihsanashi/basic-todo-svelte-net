aa697030676428fd9170f6e91f89f43937625ff6
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-27 23:26:01 +0800

    feat: fetch user onMount

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,7 +1,30 @@
 <script lang="ts">
+	import '@app/app.css';
+	import { onMount } from 'svelte';
+
 	import { Toaster } from '$lib/components/ui/sonner';
 
-	import '../app.css';
+	import { authActions } from '@api';
+	import { authStore } from '@stores/auth';
+
+	import type { GetApiAuthMeResponse } from '@references/codegen';
+
+	onMount(async () => {
+		const response = await authActions.checkAuth();
+
+		if (response.success) {
+			const user = response.data as GetApiAuthMeResponse;
+			authStore.set({
+				isAuthenticated: true,
+				user: user,
+			});
+		} else {
+			authStore.set({
+				isAuthenticated: false,
+				user: null,
+			});
+		}
+	});
 </script>
 
 <Toaster />
