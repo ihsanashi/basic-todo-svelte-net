2cec39b735751b8703b21494a11401f5a48d3e5d
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-28 19:39:58 +0800

    chore: add constants

--- client/src/references/constants/index.ts
+++ client/src/references/constants/index.ts
@@ -1,5 +1,25 @@
+import type { TodoItemDTO } from '../codegen';
+
 export * from './messages';
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 export const API_AUTH_URL = `${API_BASE_URL}/auth`;
 export const API_TODO_URL = `${API_BASE_URL}/TodoItems`;
+
+export const commonDates = [
+	{ value: 0, label: 'Today' },
+	{ value: 1, label: 'Tomorrow' },
+	{ value: 3, label: 'In 3 days' },
+	{ value: 7, label: 'In a week' },
+	{ value: 14, label: 'In 2 weeks' },
+	{ value: 30, label: 'In a month' },
+];
+
+export const defaultTodo: TodoItemDTO = {
+	id: undefined,
+	title: null,
+	description: null,
+	createdAt: undefined,
+	updatedAt: undefined,
+	isComplete: false,
+};
