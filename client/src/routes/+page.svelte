a47c13c95caebdcea74bd1d620b990132f42dc62
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-28 10:46:36 +0800

    chore: remove logout button in homepage

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -2,15 +2,12 @@
 	import { onMount } from 'svelte';
 
 	import { authActions, getAllTodos } from '@api';
-	import { authStore, setAuthState } from '@stores/auth';
-	import { Button } from '@ui/button';
+	import { authStore } from '@stores/auth';
+	import { buttonVariants } from '@ui/button';
 
 	import type { GetApiAuthMeResponse, TodoItemDTO } from '@references/codegen';
 	import { writable } from 'svelte/store';
-	import { toast } from 'svelte-sonner';
-	import { goto } from '$app/navigation';
 
-	let isLoading = writable(false);
 	let todos = writable<TodoItemDTO[]>([]);
 	onMount(async () => {
 		const response = await authActions.checkAuth();
@@ -43,48 +40,21 @@
 			todos.set(response.data || []);
 		}
 	}
-
-	async function handleLogout() {
-		isLoading.set(true);
-
-		const response = await authActions.logout();
-
-		if (response.success) {
-			toast.success(response.toast.title, {
-				description: response.toast.description,
-			});
-
-			setAuthState({
-				isAuthenticated: false,
-				user: null,
-			});
-
-			goto('/auth#login');
-		} else {
-			toast.error(response.toast.title, {
-				description: response.toast.description,
-			});
-		}
-
-		isLoading.set(false);
-	}
 </script>
 
 {#if $authStore?.isAuthenticated}
-	<main class="container mx-auto">
-		<section class="p-8">
-			<h1>Welcome back, {$authStore.user?.email}!</h1>
-			<Button loading={$isLoading} loadingText="Loading..." on:click={handleLogout}>Logout</Button>
-			<p>List of my todos</p>
-			{#if $todos.length === 0}
-				<p>No todos yet.</p>
-			{:else}
-				<ul>
-					{#each $todos as todo}
-						<li>{todo.title}</li>
-					{/each}
-				</ul>
-			{/if}
-		</section>
-	</main>
+	<section class="p-4">
+		{#if $todos.length === 0}
+			<div class="text-center text-sm">
+				<p class="text-slate-500">No todos yet.</p>
+				<a class={`${buttonVariants({ variant: 'link' })}`} href="/new">Add new?</a>
+			</div>
+		{:else}
+			<ul>
+				{#each $todos as todo}
+					<li>{todo.title}</li>
+				{/each}
+			</ul>
+		{/if}
+	</section>
 {/if}
