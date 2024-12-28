a6743002bd696a918539dcece7b99b201d15ccf0
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-29 07:35:06 +0800

    fix: remove outdated imports

--- client/src/lib/components/todo-item/index.ts
+++ client/src/lib/components/todo-item/index.ts
@@ -1,5 +1,3 @@
 import TodoItem from './todo-item.svelte';
-import Title from './todo-item-title.svelte';
-import Content from './todo-item-content.svelte';
 
-export { TodoItem, Title as TodoItemTitle, Content as TodoItemContent };
+export { TodoItem };
