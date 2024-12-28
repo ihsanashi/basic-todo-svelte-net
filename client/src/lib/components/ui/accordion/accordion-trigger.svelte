8682c6061cfb36144ab1c490cd83c68c2a301ce6
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-28 16:29:24 +0800

    fix: re-order trigger placement, place slot outside the trigger

--- client/src/lib/components/ui/accordion/accordion-trigger.svelte
+++ client/src/lib/components/ui/accordion/accordion-trigger.svelte
@@ -1,26 +1,28 @@
 <script lang="ts">
-	import { Accordion as AccordionPrimitive } from "bits-ui";
-	import ChevronDown from "lucide-svelte/icons/chevron-down";
-	import { cn } from "$lib/utils.js";
+	import { Accordion as AccordionPrimitive } from 'bits-ui';
+	import ChevronDown from 'lucide-svelte/icons/chevron-down';
+	import { cn } from '$lib/utils.js';
 
 	type $$Props = AccordionPrimitive.TriggerProps;
 	type $$Events = AccordionPrimitive.TriggerEvents;
 
-	let className: $$Props["class"] = undefined;
-	export let level: AccordionPrimitive.HeaderProps["level"] = 3;
+	let className: $$Props['class'] = undefined;
+	export let level: AccordionPrimitive.HeaderProps['level'] = 3;
 	export { className as class };
 </script>
 
 <AccordionPrimitive.Header {level} class="flex">
-	<AccordionPrimitive.Trigger
-		class={cn(
-			"flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180",
-			className
-		)}
-		{...$$restProps}
-		on:click
-	>
+	<div class="flex flex-row items-center">
+		<AccordionPrimitive.Trigger
+			class={cn(
+				'flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+				className
+			)}
+			{...$$restProps}
+			on:click
+		>
+			<ChevronDown class="transition-transform duration-200" size={16} />
+		</AccordionPrimitive.Trigger>
 		<slot />
-		<ChevronDown class="h-4 w-4 transition-transform duration-200" />
-	</AccordionPrimitive.Trigger>
+	</div>
 </AccordionPrimitive.Header>
