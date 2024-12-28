c589f086ca19227bfb8c50465c053a963c597c5f
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-29 00:02:16 +0800

    feat: update types & import paths, add function for new single todo and delete todo

--- client/src/api/todo.ts
+++ client/src/api/todo.ts
@@ -1,12 +1,8 @@
 import { API_TODO_URL } from '@references/constants';
 
-import type {
-	GetTodoItemsResponse,
-	PostTodoItemsBulkSaveResponse,
-	TodoItemDTO,
-} from '@references/codegen';
+import type { TodoItemsMultipleResponse, TodoItemDTO, TodoItemResponse } from '@references/codegen';
 
-export async function getAllTodos(): Promise<GetTodoItemsResponse> {
+export async function getAllTodos(): Promise<TodoItemsMultipleResponse> {
 	try {
 		const response = await fetch(`${API_TODO_URL}`, {
 			credentials: 'include',
@@ -17,15 +13,15 @@ export async function getAllTodos(): Promise<GetTodoItemsResponse> {
 		});
 
 		if (response.ok) {
-			const data: GetTodoItemsResponse = await response.json();
+			const json: TodoItemsMultipleResponse = await response.json();
 
 			return {
 				success: true,
-				data: data.data,
+				data: json.data,
 				errorMessage: null,
 			};
 		} else {
-			const data: GetTodoItemsResponse = await response.json();
+			const data: TodoItemsMultipleResponse = await response.json();
 
 			return {
 				success: false,
@@ -45,7 +41,7 @@ export async function getAllTodos(): Promise<GetTodoItemsResponse> {
 	}
 }
 
-export async function bulkSaveTodos(todos: TodoItemDTO[]): Promise<PostTodoItemsBulkSaveResponse> {
+export async function bulkSaveTodos(todos: TodoItemDTO[]): Promise<TodoItemsMultipleResponse> {
 	try {
 		const response = await fetch(`${API_TODO_URL}/bulk`, {
 			credentials: 'include',
@@ -57,16 +53,74 @@ export async function bulkSaveTodos(todos: TodoItemDTO[]): Promise<PostTodoItems
 		});
 
 		if (response.ok) {
-			const json = await response.json();
+			const json: TodoItemsMultipleResponse = await response.json();
 
-			return { success: true, data: json, errorMessage: null };
+			return { success: true, data: json.data, errorMessage: null };
 		} else {
-			const errorData: PostTodoItemsBulkSaveResponse = await response.json();
+			const errorData: TodoItemsMultipleResponse = await response.json();
 			return { success: false, data: null, errorMessage: errorData.errorMessage };
 		}
 	} catch (error) {
 		console.error(error);
 		const errorMessage = error instanceof Error ? error.message : 'Unknown error';
 		return { success: false, data: null, errorMessage: errorMessage };
+	}
+}
+
+export async function postSingleTodo(todo: TodoItemDTO): Promise<TodoItemResponse> {
+	try {
+		const response = await fetch(`${API_TODO_URL}`, {
+			credentials: 'include',
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(todo),
+		});
+
+		if (response.ok) {
+			const json: TodoItemResponse = await response.json();
+
+			return {
+				success: true,
+				data: json.data,
+				errorMessage: null,
+			};
+		} else {
+			const errorData: TodoItemResponse = await response.json();
+
+			return {
+				success: false,
+				data: undefined,
+				errorMessage: errorData.errorMessage,
+			};
+		}
+	} catch (error) {
+		console.error(error);
+		const errorMessage = error instanceof Error ? error.message : 'Unknown error';
+
+		return {
+			success: false,
+			data: undefined,
+			errorMessage: errorMessage,
+		};
+	}
+}
+
+export async function softDeleteTodo(id: string) {
+	try {
+		const response = await fetch(`${API_TODO_URL}/${id}`, {
+			credentials: 'include',
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		});
+
+		// if (response.ok) {
+		// }
+	} catch (error) {
+		console.error(error);
+		const errorMessage = error instanceof Error ? error.message : 'Unknown error';
 	}
 }
