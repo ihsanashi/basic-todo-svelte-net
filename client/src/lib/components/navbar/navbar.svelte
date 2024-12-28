22db4ce2153e62a23adae5f3ce5a1b8de0038682
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-28 19:41:17 +0800

    fix:  navbar enhancement

--- client/src/lib/components/navbar/navbar.svelte
+++ client/src/lib/components/navbar/navbar.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
-	import { authStore } from '@stores/auth';
 	import { buttonVariants } from '@ui/button';
+
+	import * as Avatar from '@ui/avatar';
+	import { UserIcon } from 'lucide-svelte';
 </script>
 
 <nav class="flex flex-row items-center justify-between p-4">
-	<section>
-		<a class={`${buttonVariants({ variant: 'link' })}`} href="/">Basic Todo</a>
-	</section>
-
 	<section>
 		<a
 			class={`${buttonVariants({ variant: 'link', size: 'sm' })} text-xs text-slate-600`}
 			href="/profile"
 		>
-			{$authStore?.user?.email}
+			<Avatar.Root>
+				<Avatar.Fallback class="hover:bg-slate-100">
+					<UserIcon size={16} />
+				</Avatar.Fallback>
+			</Avatar.Root>
 		</a>
 	</section>
 
 	<section>
-		<a class={buttonVariants({ variant: 'default', size: 'sm' })} href="/new">New</a>
+		<a class={`${buttonVariants({ variant: 'link' })}`} href="/">Basic Todo</a>
+	</section>
+
+	<section>
+		<a class={buttonVariants({ variant: 'outline', size: 'sm' })} href="/new">New</a>
 	</section>
 </nav>
