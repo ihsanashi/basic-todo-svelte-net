69eaaba8a07370f5ed0ad23b7cae9ecb67d2e152
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-29 09:40:34 +0800

    chore: backend build

--- web-api/obj/Debug/net9.0/web-api.AssemblyInfo.cs
+++ web-api/obj/Debug/net9.0/web-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21a750fbfdb41b8d6f226d2b8f50930860d09fa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c80b430b1fd9fd56cd6ea7b387ee1a965c6509da")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
