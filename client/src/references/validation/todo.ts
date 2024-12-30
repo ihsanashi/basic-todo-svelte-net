008812c05f7824022b4918f50f1ac86fa212e9b2
Ahmad Ihsan <ihsanashi@gmail.com>
2024-12-30 13:51:15 +0800

    feat: add function to validate todo, minor validation adjustment on dueDate

--- client/src/references/validation/todo.ts
+++ client/src/references/validation/todo.ts
@@ -5,9 +5,22 @@ export const todoSchema: z.ZodType<TodoItemDTO> = z.object({
 	id: z.number().optional(),
 	title: z.string().nonempty('Title is required'),
 	description: z.string().optional().nullable().default(''), // Allow empty string as a valid value	isComplete: z.boolean().optional(),
-	dueDate: z.string().optional(),
+	dueDate: z.string().nullable().optional(),
 	createdAt: z.string().optional(),
 	updatedAt: z.string().optional(),
 });
 
 export type TodoSchema = z.infer<typeof todoSchema>;
+
+export function validateTodoItem(todo: TodoItemDTO): boolean {
+	try {
+		todoSchema.parse(todo);
+		return true;
+	} catch (error) {
+		console.error(`Todo item failed validation check: ${error}`);
+		if (error instanceof z.ZodError) {
+			console.error(`ZodError: ${error.errors}`);
+		}
+		return false;
+	}
+}
